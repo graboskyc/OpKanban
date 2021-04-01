97eb69f4ace4ca3544cb65cd096655091daa00f1
Chris Grabosky <chris@grabosky.net>
2021-04-01 12:16:22 -0400

    stop statically defining drop downs

---
+++ OpKanban/OpKanban/Data/Dropdown.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+
+namespace OpKanban.Data
+{
+    class DDLOption {
+        public string Key {get;set;}
+        public string Value {get;set;}
+    }
+    class DDL_Color
+    {
+        private static List<DDLOption> _l = new List<DDLOption>() {
+            new DDLOption { Key="Red", Value="#E51400"},
+            new DDLOption { Key="Cyan", Value="#1BA1E2"},
+            new DDLOption { Key="Emerald", Value="#008A00"},
+            new DDLOption { Key="Green", Value="#60A917"},
+            new DDLOption { Key="Indigo", Value="#6A00FF"},
+            new DDLOption { Key="Magenta", Value="#D80073"},
+            new DDLOption { Key="Mauve", Value="#76608A"},
+            new DDLOption { Key="Orange", Value="#FA6800"}
+        };
+        public static List<DDLOption> Colors {
+            get {return _l;}
+        }
+    }
+
+    class DDL_Stage
+    {
+        private static List<DDLOption> _l = new List<DDLOption>() {
+            new DDLOption { Key="Discovery", Value="Discovery"},
+            new DDLOption { Key="Discovery", Value="Discovery"},
+            new DDLOption { Key="Sizing", Value="Sizing"},
+            new DDLOption { Key="Tech Validation", Value="Tech Validation"},
+            new DDLOption { Key="Closed Won", Value="Closed Won"},
+            new DDLOption { Key="Closed Lost", Value="Closed Lost"}
+        };
+        public static List<DDLOption> Stages {
+            get {return _l;}
+        }
+    }
+
+    class DDL_Champs
+    {
+        private static List<DDLOption> _l = new List<DDLOption>() {
+            new DDLOption { Key="Not a target", Value="Not a target"},
+            new DDLOption { Key="Target", Value="Target"},
+            new DDLOption { Key="Skeptic", Value="Skeptic"},
+            new DDLOption { Key="Champion", Value="Champion"},
+            new DDLOption { Key="Tested Champion", Value="Tested Champion"},
+            new DDLOption { Key="EB", Value="EB"},
+            new DDLOption { Key="Blocker", Value="Blocker"}
+        };
+        public static List<DDLOption> Statuses {
+            get {return _l;}
+        }
+    }
+}
