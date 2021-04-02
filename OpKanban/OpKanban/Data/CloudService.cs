ee57a5abe753c9a7105acee74810e493195cdac5
Chris Grabosky <chris@grabosky.net>
2021-04-02 15:50:59 -0400

    track links to cloud services

---
+++ OpKanban/OpKanban/Data/CloudService.cs
@@ -0,0 +1,35 @@
+using System;
+using MongoDB.Bson;
+using Realms;
+
+namespace OpKanban.Data
+{
+    public class CloudService : RealmObject
+    {
+        [PrimaryKey]
+        [MapTo("_id")]
+        public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
+
+        [MapTo("_pk")]
+        [Required]
+        public string Partition { get; set; }
+
+        [MapTo("name")]
+        [Required]
+        public string Name { get; set; }
+
+        [MapTo("link")]
+        public string Link { get; set; }
+
+        [MapTo("note")]
+        public string Note { get; set; }
+
+
+        [MapTo("color")]
+        public string Color { get; set; }
+
+        [MapTo("company")]
+        public string Company { get; set; }
+
+    }
+}
